c3d5b8bc01ebc150bf7410c335b979e88cd6b3e1
Elijah Polyakov <Elijah Polyakov>
2017-02-24 09:29:22 -0500

    initial commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    float moveUp;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, moveUp, moveVertical);
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown("space"))
+        {
+            moveUp = 5;
+        }
+        else
+        {
+            if (moveUp > -6)
+            {
+                moveUp = moveUp - .5f;
+            }
+            else
+            {
+                // because it's already falling fast, do nothing
+            }
+        }
+    }
+
+}
